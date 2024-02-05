7d58fe920cc92c4182f113a8d52cb290e212d3fa
trandung2k1 <trandungitksnb2001@gmail.com>
2024-02-05 17:09:49 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -7,26 +7,28 @@ const connectDB = require('./config/connectDB');
 const routes = require('./routes');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 // const corsOptions = {
 //   origin: '*',
 //   optionsSuccessStatus: 200,
 // };
 
-const allowlist = ['http://localhost:3000'];
-const corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
+
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptionsDelegate));
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(morgan('dev'));
 connectDB();
