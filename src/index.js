a7240ffb2c02c67443f73535d8dd18a9e599404f
trandung2k1 <trandungitksnb2001@gmail.com>
2024-09-17 11:15:23 +0700

    update dynamic cors and disable warning mongoose

--- src/index.js
+++ src/index.js
@@ -8,26 +8,29 @@ const routes = require("./routes");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-
-// const whitelist = ['http://localhost:3000', 'https://spectacular-blini-a8571f.netlify.app'];
 // const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
+//   origin: "*",
+//   optionsSuccessStatus: 200,
 // };
+
+const whitelist = [
+  "http://localhost:3000",
+  "https://spectacular-blini-a8571f.netlify.app",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(helmet());
