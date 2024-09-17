5abe3db342be4b3c51617bfb990ef6734548bf32
trandung2k1 <trandungitksnb2001@gmail.com>
2024-09-17 16:56:10 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -8,32 +8,38 @@ const routes = require("./routes");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const nocache = require("nocache");
 // const corsOptions = {
 //   origin: "*",
 //   optionsSuccessStatus: 200,
 // };
-
 const whitelist = [
   "http://localhost:3000",
+  "http://localhost:4200",
   "https://spectacular-blini-a8571f.netlify.app",
 ];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    console.log("Request origin:", origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true,
+  exposedHeaders: ["Authorization"],
+  allowedHeaders: ["Authorization"],
 };
-
-const app = express();
 const port = process.env.PORT || 4000;
+const app = express();
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cors(corsOptions));
-app.use(cookieParser());
+app.disable("x-powered-by");
+app.use(nocache());
 app.use(helmet());
+app.use(cookieParser());
 app.use(morgan("combined"));
 connectDB();
 app.use("/api/v1/", routes);
